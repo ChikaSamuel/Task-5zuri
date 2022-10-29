589209895d62a13f909fb082a3f955191490a6c5
chikasamuel <samuelmereal@gmail.com>
2022-10-29 12:55:54 +0100

    upload

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const http = require('http');
 
 const server = http.createServer((request, response) =>{
     
-        let filePath = path.join (__dirname, 'pfiles', request.url === '/' ? 'home.html': request.url)
+        let filePath = path.join (__dirname, 'HTMLfiles', request.url === '/' ? 'home.html': request.url)
         fs.readFile (filePath, 'utf8', (err, data) =>{
             response.writeHead(200, {'Content-Type': 'text/html'})
             response.end(data)
         }) 
 
     if (request.url === '/about.html'){
-        let filePath = path.join (__dirname, 'pfiles', 'about.html')
+        let filePath = path.join (__dirname, 'HTMLfiles', 'about.html')
         fs.readFile (filePath, 'utf8', (err, data) =>{
             response.writeHead(200, {'Content-Type': 'text/html'})
             response.end(data)
-        })
+        })  
     }
     if (request.url === '/contact.html'){
-        let filePath = path.join (__dirname, 'pfiles', 'contact.html')
+        let filePath = path.join (__dirname, 'HTMLfiles', 'contact.html')
         fs.readFile (filePath, 'utf8', (err, data) =>{
             response.writeHead(200, {'Content-Type': 'text/html'})
             response.end(data)
